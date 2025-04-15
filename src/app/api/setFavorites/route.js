ec690e32ce2a360b8b4c8dd49bb5754905d4e7a4
jpprestes1 <joao.p.m.prestes@gmail.com>
2025-04-15 15:34:02 -0300

    ajustado

--- src/app/api/setFavorites/route.js
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server'
-
-export async function POST(req) {
-  const { bookmark } = await req.json()
-
-  localStorage.setItem("fortniteBase", JSON.stringify(bookmark));
-
-  return NextResponse.json({ error: 'Credenciais inválidas' }, { status: 401 })
-}
